7b9b88455c5121259cef8b382dee2e3bbfa4aef3
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-01-23 14:52:15 -0600

    deleted code from previous branch that is no longer needed

--- src/scripts/entryComponent.js
+++ src/scripts/entryComponent.js
@@ -8,12 +8,3 @@ const build = {
         `
      }
 };
-
-// const journalEntryFactory = (entry) => {
-//     return `
-//     <h3 class="concepts">${entry.concepts}</h3>
-//     <p class="date">${entry.date}</p>
-//     <p class="entry">${entry.entry}</p>
-//     <p class="mood">Mood: ${entry.mood}</p> 
-//     `
-//  };
