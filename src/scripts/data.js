2dd0cbabc6591398245b18fb3521af11d933dca0
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-02-04 15:22:22 -0600

    reverted part_seven back to code in part_six to start over

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,18 +1,8 @@
-const entryUrl = "http://localhost:8088/journalEntries"
-
 const apiManager = {
     getEntries() {
+        const entryUrl = "http://localhost:8088/journalEntries"
         return fetch(entryUrl)
             .then(resp => resp.json())
-    },
-    saveJournalEntry(newJournalEntry) {
-        fetch(entryUrl, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(newJournalEntry)
-        })
     }
 };
 
--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,31 +1,4 @@
 import apiManager from './data.js'
 import render from './entriesDOM.js'
-import entryManager from './entryManager.js';
-
-const entryManager = {
-    addSubmitClickEventListener() {
-        const recordEntryButton = document.querySelector(".button");
-
-        recordEntryButton.addEventListener("click", () => {
-            
-            const userDateInput = document.getElementById("Date");
-            const userConceptsInput = document.getElementById("Concepts");
-            const userEntryInput = document.getElementById("Entry");
-            const userMoodInput = document.getElementById("Mood");
-
-            const entryArray = [userDateInput.value, userConceptsInput.value, userEntryInput.value, userMoodInput.value];
-
-            
-
-
-
-
-            });
-        }
-    };
 
 apiManager.getEntries().then(render.renderJournalEntries);
-
-entryManager.addSubmitClickEventListener();
-
-
