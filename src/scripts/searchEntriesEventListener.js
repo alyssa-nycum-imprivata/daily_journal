dcc1c851a53ee79516d8c380cb1daa9c97ddc3ae
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-02-12 08:36:31 -0600

    finished daily journal 11 - made function search input field

--- src/scripts/searchEntriesEventListener.js
+++ src/scripts/searchEntriesEventListener.js
@@ -4,22 +4,35 @@ import render from "./renderJournalEntries.js";
 const searchEntriesEventListener = () => {
     const searchField = document.getElementById("search_entries");
 
-    searchField.addEventListener("keyup", () => {
-        
-        const userInput = searchField.value;
+    searchField.addEventListener("keyup", event => {
+        if (event.keyCode === 13) {
+            const entryLogContainer = document.querySelector(".entryLog");
 
-        apiManager.getAllEntries()
-            .then(entries => {
-                filteredEntries = entries.filter(entry => {
-                    if (event.keyCode === 13) {
-                        for (let entry of Object.values(userInput)) {
-                            return entry
+            entryLogContainer.innerHTML = "";
+
+            let userInput = event.target.value
+
+            apiManager.getAllEntries()
+                .then(entries => {
+                    const searchEntries = entries.filter(entry => {
+                        for (const value of Object.values(entry)) {
+                            const valueType = typeof value
+                            if (valueType === "string" && value.includes(userInput)) {
+                                return entry
+                            }
                         }
-                    }
+                    })
+                    render.renderJournalEntries(searchEntries)
+
+                    searchField.value = "";
                 })
-                render.renderJournalEntries(filteredEntires)
-            })
+        }
+
     })
 };
 
 export default searchEntriesEventListener;
+
+
+
+
